fb8b9d24e276a43bb979d2bf0208ff6762858cff
victor-ugwueze <victorugwueze@gmail.com>
2020-03-29 00:48:58 +0100

    fix episode as subtitle

--- src/components/CardEpisode/index.js
+++ src/components/CardEpisode/index.js
@@ -4,7 +4,6 @@ import ReactHtmlParser from 'react-html-parser';
 
 export default function MovieCard({ episode }) {
   const { image } = episode;
-  const regex = /(<p>)|(<\/p>)/gi;
   return (
    <a href={episode.url}>
      <div className="card">
@@ -16,18 +15,9 @@ export default function MovieCard({ episode }) {
       : <div className="card-no-img"> No preview image</div>
     }
       <div className="card-body">
-        <h5 className="card-title">{episode.name}</h5>
-       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Season {episode.season} Episode {episode.number}</li>
-        <li className="list-group-item">AirDate: {episode.airdate}</li>
-      </ul>
-        <p className="card-text">
-          {/* {episode.summary
-            ? `${episode.summary.substring(0, 255).replace(regex, '\n')}...`
-            : ''}
-            <a href={episode.url} >See more</a> */}
-        </p>
-        {ReactHtmlParser(episode.summary)}
+        <h5 className="card-title">{episode.name} ({episode.airdate.split('-')[0]})</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Season {episode.season} Episode {episode.number}</h6>
+        {episode.summary && ReactHtmlParser(episode.summary)}
       </div>
     </div>
    </a>
